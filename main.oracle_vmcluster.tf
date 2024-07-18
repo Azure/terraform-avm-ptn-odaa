e4f5fc9199783514a454cbcaa3125cb83d4b16c8
Gerardo Reyes <epalnemohuani@hotmail.com>
2024-07-18 14:11:15 -0400

    Adding variables

--- main.oracle_vmcluster.tf
+++ main.oracle_vmcluster.tf
@@ -7,7 +7,7 @@ resource "azapi_resource" "cloudVmCluster" {
 
   type                      = "Oracle.Database/cloudVmClusters@2023-09-01-preview"
   parent_id                 = azurerm_resource_group.rg.id
-  name                      = "odaa-cluster"
+  name                      = "odaa-cluster-${local.prefix}"
   schema_validation_enabled = false
 
   timeouts {
@@ -17,52 +17,52 @@ resource "azapi_resource" "cloudVmCluster" {
 
   body = jsonencode({
     "properties" : {
-      "dataStorageSizeInTbs" : 1000,
-      "dbNodeStorageSizeInGbs" : 1000,
-      "memorySizeInGbs" : 1000,
-      "timeZone" : "UTC",
-      "hostname" : "hostname1",
-      "domain" : "domain1",
-      "cpuCoreCount" : 2,
-      "ocpuCount" : 3,
-      "clusterName" : "cluster1",
-      "dataStoragePercentage" : 100,
-      "isLocalBackupEnabled" : false,
+      "dataStorageSizeInTbs" : var.cloud_exadata_vm_cluster.data_storage_size_in_tbs,
+      "dbNodeStorageSizeInGbs" : var.cloud_exadata_vm_cluster.dbnode_storage_size_in_gbs,
+      "memorySizeInGbs" : var.cloud_exadata_vm_cluster.memory_size_in_gbs,
+      "timeZone" : var.cloud_exadata_vm_cluster.time_zone,
+      "hostname" : var.cloud_exadata_vm_cluster.hostname,
+      #"domain" : "domain1",
+      "cpuCoreCount" : var.cloud_exadata_vm_cluster.cpu_core_count,
+      #"ocpuCount" : 3,
+      "clusterName" : var.cloud_exadata_vm_cluster.name,
+      "dataStoragePercentage" : var.cloud_exadata_vm_cluster.data_storage_percentage,
+      "isLocalBackupEnabled" : var.cloud_exadata_vm_cluster.is_local_backup_enabled,
       "cloudExadataInfrastructureId" : "${azapi_resource.odaa_infra.id}",
-      "isSparseDiskgroupEnabled" : false,
+      "isSparseDiskgroupEnabled" : var.cloud_exadata_vm_cluster.is_sparse_diskgroup_enabled,
       "sshPublicKeys" : [
-        "ssh-key 1"
+        var.cloud_exadata_vm_cluster.ssh_public_keys
       ],
-      "nsgCidrs" : [
-        {
-          "source" : "10.0.0.0/16",
-          "destinationPortRange" : {
-            "min" : 1520,
-            "max" : 1522
-          }
-        },
-        {
-          "source" : "10.10.0.0/24"
-        }
-      ],
-      "licenseModel" : "LicenseIncluded",
-      "scanListenerPortTcp" : 1050,
-      "scanListenerPortTcpSsl" : 1025,
-      "vnetId" : "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg000/providers/Microsoft.Network/virtualNetworks/vnet1",
-      "giVersion" : "19.0.0.0",
-      "subnetId" : "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg000/providers/Microsoft.Network/virtualNetworks/vnet1/subnets/subnet1",
-      "backupSubnetCidr" : "172.17.5.0/24",
+      # "nsgCidrs" : [
+      #   {
+      #     "source" : "10.0.0.0/16",
+      #     "destinationPortRange" : {
+      #       "min" : 1520,
+      #       "max" : 1522
+      #     }
+      #   },
+      #   {
+      #     "source" : "10.10.0.0/24"
+      #   }
+      # ],
+      "licenseModel" : var.cloud_exadata_vm_cluster.license_model,
+      #"scanListenerPortTcp" : 1050,
+      #"scanListenerPortTcpSsl" : 1025,
+      "vnetId" : var.cloud_exadata_vm_cluster.vnet_id,
+      "giVersion" : var.cloud_exadata_vm_cluster.gi_version,
+      "subnetId" : var.cloud_exadata_vm_cluster.subnet_id,
+      #"backupSubnetCidr" : "172.17.5.0/24",
       "dataCollectionOptions" : {
-        "isDiagnosticsEventsEnabled" : false,
-        "isHealthMonitoringEnabled" : false,
-        "isIncidentLogsEnabled" : false
+        "isDiagnosticsEventsEnabled" : var.cloud_exadata_vm_cluster.is_diagnostic_events_enabled,
+        "isHealthMonitoringEnabled" : var.cloud_exadata_vm_cluster.is_health_monitoring_enabled,
+        "isIncidentLogsEnabled" : var.cloud_exadata_vm_cluster.is_incident_logs_enabled
       },
-      "displayName" : "cluster 1",
-      "dbServers" : [
-        "ocid1..aaaa"
-      ]
+      "displayName" : var.cloud_exadata_vm_cluster.display_name,
+      # "dbServers" : [
+      #   "ocid1..aaaa"
+      # ]
     },
-    "location" : "eastus"
+    "location" : var.location
     }
   )
   response_export_values = ["properties.ocid"]
